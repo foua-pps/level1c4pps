7c43ab3052110beebc9dc011afd8274706c17072
Adam Dybbroe <Adam.Dybbroe@smhi.se>
2019-09-17 13:38:40 +0200

    Make a python package out of the scripts
    Signed-off-by: Adam Dybbroe <Adam.Dybbroe@smhi.se>

---
+++ setup.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2019 Pytroll
+
+# Author(s):
+
+#   Nina Håkansson <nina.hakansson@smhi.se>
+#   Erik Johansson <erik.johansson@smhi.se>
+#   Adam Dybbroe <adam.dybbroe@smhi.se>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Scripts to convert various satellite level-1 data formats (e.g. SEVIRI level
+1.5 HRIT) to NWCSAF/PPS level-1c format
+
+"""
+
+from setuptools import find_packages, setup
+
+try:
+    # HACK: https://github.com/pypa/setuptools_scm/issues/190#issuecomment-351181286
+    # Stop setuptools_scm from including all repository files
+    import setuptools_scm.integration
+    setuptools_scm.integration.find_files = lambda _: []
+except ImportError:
+    pass
+
+
+NAME = "level1c4pps"
+README = open('README.md', 'r').read()
+
+setup(name=NAME,
+      # version=version.__version__,
+      description='Tools to convert various satellite level-1 data formats to NWCSAF/PPS level-1c format',
+      long_description=README,
+      author='Nina Hakansson',
+      author_email='nina.hakansson@smhi.se',
+      classifiers=["Development Status :: 3 - Alpha",
+                   "Intended Audience :: Science/Research",
+                   "License :: OSI Approved :: GNU General Public License v3 " +
+                   "or later (GPLv3+)",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   "Topic :: Scientific/Engineering"],
+      url="https://github.com:foua-pps/level1c4pps",
+      packages=['level1c4pps', ],
+      scripts=['bin/seviri2pps.py', 'bin/gac2pps.py', ],
+      data_files=[],
+      zip_safe=False,
+      use_scm_version=True,
+      python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
+      install_requires=['satpy', 'pyorbital', 'trollsift', ],
+      )
