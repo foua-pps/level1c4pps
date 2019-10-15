fcb44a704619efa3b80e9a30fc8896b30742b2fa
Nina Hakansson <nina.hakansson@smhi.se>
2019-10-15 12:14:21 +0200

    Updated valid_range

--- level1c4pps/gac2pps_lib.py
+++ level1c4pps/gac2pps_lib.py
@@ -29,6 +29,7 @@ import os
 import time
 import xarray as xr
 import dask.array as da
+import numpy as np
 from datetime import datetime
 from satpy.scene import Scene
 from level1c4pps import dt64_to_datetime
@@ -163,7 +164,7 @@ def process_one_file(gac_file, out_path='.', reader_kwargs=None):
     del scn_['solar_azimuth_angle']
     scn_['sunazimuth'].attrs['id_tag'] = 'sunazimuth'
     scn_['sunazimuth'].attrs['long_name'] = 'sun azimuth angle degree clockwise from north'
-    scn_['sunazimuth'].attrs['valid_range'] = [0, 18000]
+    scn_['sunazimuth'].attrs['valid_range'] = [-18000, 18000]
     scn_['sunazimuth'].attrs['name'] = "image{:d}".format(image_num)
     angle_names.append("image{:d}".format(image_num))
     scn_['sunazimuth'].attrs['coordinates'] = 'lon lat'
@@ -177,7 +178,7 @@ def process_one_file(gac_file, out_path='.', reader_kwargs=None):
     del scn_['sensor_azimuth_angle']
     scn_['satazimuth'].attrs['id_tag'] = 'satazimuth'
     scn_['satazimuth'].attrs['long_name'] = 'satellite azimuth angle degree clockwise from north'
-    scn_['satazimuth'].attrs['valid_range'] = [0, 9000]
+    scn_['satazimuth'].attrs['valid_range'] = [-18000, 18000]
     scn_['satazimuth'].attrs['name'] = "image{:d}".format(image_num)
     angle_names.append("image{:d}".format(image_num))
     scn_['satazimuth'].attrs['coordinates'] = 'lon lat'
@@ -187,7 +188,6 @@ def process_one_file(gac_file, out_path='.', reader_kwargs=None):
     image_num += 1
 
     # scanline timestamps
-    import numpy as np
     first_jan_1970 = np.array([datetime(1970, 1, 1, 0, 0, 0)]).astype('datetime64[ns]')
     scanline_timestamps = np.array(scn_['qual_flags'].coords['acq_time'] -
                                    first_jan_1970).astype(dtype='timedelta64[ms]').astype(np.float64)
@@ -258,7 +258,6 @@ def process_one_file(gac_file, out_path='.', reader_kwargs=None):
                                         'complevel': 4}
 
     header_attrs = scn_.attrs.copy()
-    print(type(irch.attrs['start_time']))
     header_attrs['start_time'] = datetime.strftime(dt64_to_datetime(irch.attrs['start_time']),
                                                    "%Y-%m-%d %H:%M:%S")
     header_attrs['end_time'] = datetime.strftime(dt64_to_datetime(irch.attrs['end_time']),
@@ -280,10 +279,10 @@ def process_one_file(gac_file, out_path='.', reader_kwargs=None):
                     scn_[band].attrs[attr+str(key)] = attr_dict[key]
         except KeyError:
             continue
-
     scn_.save_datasets(writer='cf', filename=filename,
                        header_attrs=header_attrs, engine='netcdf4',
                        encoding=save_info)
+
     print("Saved file {:s} after {:3.1f} seconds".format(
         os.path.basename(filename),
         time.time()-tic))
