a1f04cf2854c52accde7d96f0a96f511432e7069
Nina Hakansson <nina.hakansson@smhi.se>
2019-11-13 16:21:43 +0100

    Remove acq_time as before

--- level1c4pps/gac2pps_lib.py
+++ level1c4pps/gac2pps_lib.py
@@ -138,6 +138,7 @@ def set_header_and_band_attrs(scene):
             scene[band].attrs['coordinates'] = 'lon lat'
             # Add time coordinate. To make cfwriter aware that we want 3D data.
             scene[band].coords['time'] = irch.attrs['start_time']
+            del scene[band].coords['acq_time']
             del scene[band].attrs['area']
         except KeyError:
             continue
