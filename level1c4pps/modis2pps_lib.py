eff5ca084477876cfd7e52536ac35498a3b27e5c
Nina Hakansson <nina.hakansson@smhi.se>
2020-01-10 16:22:02 +0100

    Added support for modis

--- level1c4pps/modis2pps_lib.py
+++ level1c4pps/modis2pps_lib.py
@@ -22,7 +22,7 @@
 #   Nina Hakansson <nina.hakansson@smhi.se>
 #   Adam.Dybbroe <adam.dybbroe@smhi.se>
 
-"""Functions to convert MERSI-2 level-1 data to a NWCSAF/PPS level-1c formatet netCDF/CF file."""
+"""Functions to convert MODIS level-1 data to a NWCSAF/PPS level-1c formatet netCDF/CF file."""
 
 import os
 import time
@@ -31,34 +31,31 @@ from satpy.scene import Scene
 from level1c4pps import (make_azidiff_angle, get_encoding, compose_filename,
                          ANGLE_ATTRIBUTES, rename_latitude_longitude,
                          update_angle_attributes, get_header_attrs, 
-                         SATPY_ANGLE_NAMES, convert_angles)
+                         SATPY_ANGLE_NAMES, convert_angles, 
+                         apply_sunz_correction)
 import pyspectral  # testing that pyspectral is available # noqa: F401
 import logging
 
 # Example:
 # tf2019234102243.FY3D-X_MERSI_GEOQK_L1B.HDF
 # tf2019234102243.FY3D-X_MERSI_GEO1K_L1B.HDF
-# tf2019234102243.FY3D-X_MERSI_1000M_L1B.HDF
-# tf2019234102243.FY3D-X_MERSI_0250M_L1B.HDF
-#
 
-logger = logging.getLogger('mersi22pps')
 
-PLATFORM_SHORTNAMES = {'FY3D': 'FY-3D'}
-# BANDNAMES = ['%d' % (chn+1) for chn in range(25)]
-BANDNAMES = ['3', '4', '5', '6', '20', '23', '24', '25']
+logger = logging.getLogger('modis2pps')
 
-PPS_TAGNAMES = {'3': 'ch_r06',
-                '4': 'ch_r09',
-                '5': 'ch_r13',
+BANDNAMES = ['1', '2', '6', '20', '26', '28', '29', '31', '32']
+
+REFL_BANDS = ['1', '2', '6','26']
+
+PPS_TAGNAMES = {'1': 'ch_r06',
+                '2': 'ch_r09',
+                '26': 'ch_r13',
                 '6': 'ch_r16',
                 '20': 'ch_tb37',
-                '22': 'ch_tb73',
-                '23': 'ch_tb85',
-                '24': 'ch_tb11',
-                '25': 'ch_tb12'}
-
-MERSI2_LEVEL1_FILE_PATTERN = 'tf{start_time:%Y%j%H%M%S}.{platform_shortname:4s}-X_MERSI_{dataset}_L1B.HDF'
+                '28': 'ch_tb73',
+                '29': 'ch_tb85',
+                '31': 'ch_tb11',
+                '32': 'ch_tb12'}
 
 def get_encoding_mersi2(scene):
     """Get netcdf encoding for all datasets."""
@@ -71,24 +68,21 @@ def set_header_and_band_attrs(scene):
     """Set and delete some attributes."""
     nimg = 0  # name of first dataset is image0
     # Set some header attributes:
-    irch = scene['24']
+    irch = scene['31']
     scene.attrs['platform'] = irch.attrs['platform_name']
     sensor_name = [x for x in scene.attrs['sensor']][0]
     scene.attrs['instrument'] = sensor_name.upper()
-    scene.attrs['source'] = "mersi22pps.py"
-    # Perhaps one can get the orbit number from the hdf file?
-    # FIXME!
-    scene.attrs['orbit_number'] = 99999
+    scene.attrs['source'] = "modis2pps.py"    
     nowutc = datetime.utcnow()
     scene.attrs['date_created'] = nowutc.strftime("%Y-%m-%dT%H:%M:%SZ")
-
+    scene.attrs['orbit_number'] = 00000
     # bands
     for band in BANDNAMES:
         idtag = PPS_TAGNAMES.get(band, None)
         if not idtag:
             continue
         scene[band].attrs['id_tag'] = idtag
-        scene[band].attrs['description'] = 'MERSI-2 band ' + str(band)
+        scene[band].attrs['description'] = 'MODIS band ' + str(band)
         scene[band].attrs['sun_earth_distance_correction_applied'] = 'False'
         scene[band].attrs['sun_earth_distance_correction_factor'] = 1.0
         scene[band].attrs['sun_zenith_angle_correction_applied'] = 'False'
@@ -108,31 +102,35 @@ def process_one_scene(scene_files, out_path):
     """Make level 1c files in PPS-format."""
     tic = time.time()
     scn_ = Scene(
-        reader='mersi2_l1b',
+        reader='modis_l1b',
         filenames=scene_files)
 
     scn_.load(BANDNAMES + ['latitude', 'longitude',
                            'satellite_zenith_angle', 'solar_zenith_angle',
                            'satellite_azimuth_angle', 'solar_azimuth_angle'], resolution=1000)
     # one ir channel
-    irch = scn_['24']
+    irch = scn_['31']
 
     # Set header and band attributes
     set_header_and_band_attrs(scn_)
 
     # Rename longitude, latitude to lon, lat.
     rename_latitude_longitude(scn_)
+    print(scn_)
 
     # Convert angles to PPS
     convert_angles(scn_, SATPY_ANGLE_NAMES)
     update_angle_attributes(scn_, irch)
-    for angle in ['sunzenith', 'satzenith', 'azimuthdiff']:
-        scn_[angle].attrs['file_key'] = ANGLE_ATTRIBUTES['mersi2_file_key'][angle]
 
-    filename = compose_filename(scn_, out_path, instrument='mersi2', band=irch)
+    # Apply sunz correction
+    apply_sunz_correction(scn_, REFL_BANDS)
+
+    filename = compose_filename(scn_, out_path, instrument='modis', band=irch)
+    filename = filename.replace('aqua', '2')
+    filename = filename.replace('aqua', '2')
     scn_.save_datasets(writer='cf',
                        filename=filename,
-                       header_attrs=get_header_attrs(scn_, band=irch, sensor='mersi-2'),
+                       header_attrs=get_header_attrs(scn_, band=irch, sensor='modis'),
                        engine='netcdf4',
                        include_lonlats=False,
                        flatten_attrs=True,
