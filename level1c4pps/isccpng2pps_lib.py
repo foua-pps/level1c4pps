10da412113d5dc243b15aafca8c837ffaae76130
Nina.Hakansson <6160529+ninahakansson@users.noreply.github.com>
2024-11-29 12:17:46 +0100

    Add homogenization

--- level1c4pps/isccpng2pps_lib.py
+++ level1c4pps/isccpng2pps_lib.py
@@ -39,12 +39,6 @@ from level1c4pps import (get_encoding,
 
 import logging
 
-# from satpy.utils import debug_on
-# debug_on()
-
-# Example:
-# 'SEVIRI-like_ISCCP-NG_L1g_demo_v2_res_0_05deg__temp_13_30um__20200701T1200.nc'
-
 
 logger = logging.getLogger('isccpng2pps')
 
@@ -56,7 +50,7 @@ BANDNAMES = ['refl_01_60um',
              'temp_09_70um',
              'temp_11_00um',
              'temp_12_00um',
-             'temp_13_30um',                    
+             'temp_13_30um',
              'temp_06_20um',
              'temp_07_30um']
 
@@ -76,11 +70,41 @@ PPS_TAGNAMES = {'refl_01_60um': "ch_r16",
                 'temp_09_70um': "ch_tbxx",
                 'temp_11_00um': "ch_tb11",
                 'temp_12_00um': "ch_tb12",
-                'temp_13_30um': "ch_tb133",                    
+                'temp_13_30um': "ch_tb133",
                 'temp_06_20um': "ch_tbxx",
                 'temp_07_30um': "ch_tb73"}
 
 BANDNAMES = list(PPS_TAGNAMES.keys())
+
+coef_slope_chan = ['refl_00_65um', 'refl_00_86um', 'refl_01_60um',
+                   'temp_03_80um', 'temp_06_20um', 'temp_07_30um', 'temp_08_60um',
+                   'temp_09_70um', 'temp_11_00um', 'temp_12_00um', 'temp_13_30um']
+coef_slope_list = {
+    '270_day': [1.00152, 0.946758, 1.00669, 0.926764, 0.991329, 0.999300, 1.01977, 1.03453, 0.996781, 1.02343, 0.863748],
+    '270_nig': [1.00000, 1.00000, 1.00000, 0.950274, 0.989769, 1.00713, 1.01844, 1.01421, 0.995356, 1.02101, 0.905866],
+    '271_day': [1.00298, 0.946479, 1.00762, 0.928358, 0.991436, 0.999576, 1.01939, 1.03306, 0.996656, 1.02405, 0.894260],
+    '271_nig': [1.00000, 1.00000, 1.00000, 0.950015, 0.989834, 1.00698, 1.01804, 1.01361, 0.995194, 1.02155, 0.930947],
+    '173_day': [1.00164, 0.944191, 1.00680, 0.929371, 1.00905, 1.00090, 1.01065, 1.02516, 0.996015, 1.02885, 0.871186],
+    '173_nig': [1.00000, 1.00000, 1.00000, 0.950006, 1.00397, 1.00545, 1.00990, 1.01022, 0.994367, 1.02563, 0.912099],
+    '55_day': [0.999586, 0.999657, 0.997100, 1.01192, 1.00193, 1.00093, 1.00202, 0.998663, 0.999412, 0.999663, 0.945356],
+    '55_nig': [1.00000, 1.00000, 1.00000, 1.00772, 1.00129, 1.00127, 1.00180, 0.999506, 0.999569, 0.999708, 0.961137],
+}
+coef_offset_list = {
+    '270_day': [0.000369844, 0.193868, 0.708022, 18.4795, 1.58729, -0.0312020, -4.93224, -7.83331, 1.13395, -5.51940, 30.5574],
+    '270_nig': [0.00000, 0.00000, 0.00000, 11.8884, 1.97083, -2.13396, -4.75410, -2.48929, 1.46463, -4.95287, 19.1043],
+    '271_day': [-0.00818343, 0.194117, 0.718540, 17.7929, 1.57229, -0.0877037, -4.83326, -7.49875, 1.17318, -5.66102, 23.6271],
+    '271_nig': [0.00000, 0.00000, 0.00000, 11.9196, 1.96629, -2.07708, -4.64315, -2.38190, 1.51330, -5.07539, 13.6827],
+    '173_day': [-4.45259e-05, 0.214025, 0.703233, 17.3952, -2.21718, -0.347357, -2.62520, -5.68860, 1.37321, -6.76384, 28.8614],
+    '173_nig': [0.00000, 0.00000, 0.00000, 11.9012, -0.963594, -1.56746, -2.52102, -1.75993, 1.76101, -5.99385, 17.7467],
+    '55_day': [0.00410180, 0.0160834, 0.101325, -2.83943, -0.417072, -0.181784, -0.486767, 0.304194, 0.134653, 0.0706959, 12.3763],
+    '55_nig': [0.00000, 0.00000, 0.00000, -1.61703, -0.258592, -0.278584, -0.442302, 0.0806762, 0.0877965, 0.0597462, 8.17879]
+}
+
+satellite_names = {270: "GOES-16",  # ABI
+                   271: "GOES-17",  # ABI
+                   173: "Himawari-8",  # AHI
+                   55: "Meteosat-8",  # MSG1 SEVIRI
+                   70: "Meteosat-11"}
 
 
 def get_encoding_isccpng(scene):
@@ -103,15 +127,40 @@ def set_header_and_band_attrs(scene, orbit_n=00000):
     for band in REFL_BANDS:
         if band not in scene:
             continue
-        print("Is this correct, we think so")
         scene[band].attrs['sun_zenith_angle_correction_applied'] = 'False'
     return nimg
 
+
+def homogenize_channel(scene, wmo_id, illum, band, sol_zen):
+    """Homogenize one satellite for one band."""
+    data = scene[band]
+    dwmo_id = scene["wmo_id"]
+    ch_index = coef_slope_chan.index(band)
+    k = coef_slope_list[str(wmo_id) + illum][ch_index]
+    c = coef_offset_list[str(wmo_id) + illum][ch_index]
+    logger.info(
+        f"Homogenizing channel {band} for {satellite_names[wmo_id]} for illum:{illum}"
+        f" with respect to SEVIRI on MSG4 using y={k} * x + {c}")
+    dn_discr = 90
+    if (illum == '_day'):
+        update = ((dwmo_id == wmo_id) & (data > 0) & (sol_zen < dn_discr))
+        scene[band].where(update,  scene[band] * k + c)
+    if (illum == '_nig'):
+        update = ((dwmo_id == wmo_id) & (data > 0) & (sol_zen >= dn_discr))
+        scene[band].where(update,  scene[band] * k + c)
+
+
 def homogenize(scene):
-    pass
+    """Homogenize data to Meteosat-11."""
+    sol_zen = scene["sunzenith"]
+    for band in BANDNAMES:
+        for wmo_id in [270, 271, 173, 55]:
+            homogenize_channel(scene, wmo_id, '_day', band, sol_zen)
+            homogenize_channel(scene, wmo_id, '_nig', band, sol_zen)
+
 
 def recalibrate_meteosat(scene):
-    """nominal calibration is applied, redo with meirnik calibration."""
+    """Nominal calibration is applied, redo with meirnik calibration."""
     from satpy.readers.seviri_base import MeirinkCalibrationHandler
     start_time = dt64_to_datetime(scene["refl_00_65um"].attrs["start_time"])
     channel_name = {"refl_00_65um": "VIS006",
@@ -123,12 +172,11 @@ def recalibrate_meteosat(scene):
     calibration_nominal = {2021: {321: {"VIS006": 24.974,
                                         "VIS008": 32.377,
                                         "IR_016": 23.710},
-                                  324: {"VIS006": 21.241,  
+                                  324: {"VIS006": 21.241,
                                         "VIS008": 27.921,
                                         "IR_016": 23.112}
-                                   }}
-    
-    for wmo_id in platform_id:        
+                                  }}
+    for wmo_id in platform_id:
         for band in channel_name:
             old_gain = calibration_nominal[start_time.year][platform_id[wmo_id]][channel_name[band]]
             meirink = MeirinkCalibrationHandler(calib_mode="MEIRINK-2023")
@@ -136,13 +184,13 @@ def recalibrate_meteosat(scene):
             scene[band] = scene[band] * new_gain / old_gain
 
 
-
 def fix_pixel_time(scene):
     """Fix the time pixel variable, original file does not contain units."""
     del scene["pixel_time"].coords["crs"]
     scene["pixel_time"].encoding['coordinates'] = "lon lat"
     scene["pixel_time"].data = scene["pixel_time"].data * np.timedelta64(1, 's') + scene['temp_11_00um'].attrs["start_time"]
-    
+
+
 def process_one_scene(scene_files, out_path,
                       engine='h5netcdf',
                       orbit_n=0):
@@ -163,7 +211,7 @@ def process_one_scene(scene_files, out_path,
     apply_sunz_correction(scn_, REFL_BANDS)
     recalibrate_meteosat(scn_)
     homogenize(scn_)
-    
+
     filename = compose_filename(scn_, out_path, instrument='seviri', band=irch)
     encoding = get_encoding_isccpng(scn_)
 
@@ -174,7 +222,7 @@ def process_one_scene(scene_files, out_path,
                        include_lonlats=False,
                        flatten_attrs=True,
                        encoding=encoding)
-    print("Saved file {:s} after {:3.1f} seconds".format(
+    logging.info("Saved file {:s} after {:3.1f} seconds".format(
         os.path.basename(filename),
         time.time()-tic))
     return filename
