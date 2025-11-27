3bed2ff940e1a6d3afa1871b3074f2045a4628f3
Nina.Hakansson <6160529+ninahakansson@users.noreply.github.com>
2025-11-27 12:51:12 +0100

    Updated wrong file in last commit
    Undo and commit correct file

--- level1c4pps/tests/test_seviri2pps.py
+++ level1c4pps/tests/test_seviri2pps.py
@@ -33,7 +33,6 @@ from satpy import Scene
 import level1c4pps.seviri2pps_lib as seviri2pps
 import level1c4pps.calibration_coefs as calib
 from satpy.dataset.dataid import WavelengthRange
-from pyresample import SwathDefinition
 
 
 def get_fake_scene():
@@ -46,25 +45,15 @@ def get_fake_scene():
         attrs={'calibration': 'reflectance',
                'sun_earth_distance_correction_applied': True,
                'start_time': start_time,
-               'wavelength': WavelengthRange(0.56, 0.635, 0.71),
-               }
+               'wavelength': WavelengthRange(0.56, 0.635, 0.71)}
     )
-    lons = xr.DataArray(
-        [[75, 76],
-         [77, 78]],
-        dims=('y', 'x'))
-    lats = xr.DataArray(
-        [[35, 36],
-         [37, 38]],
-        dims=('y', 'x'))
-    scene['IR_105'] = xr.DataArray(
+    scene['IR_108'] = xr.DataArray(
         [[5, 6],
          [7, 8]],
         dims=('y', 'x'),
         attrs={'calibration': 'brightness_temperature',
                'start_time': start_time,
-               'wavelength': WavelengthRange(9.8, 10.8, 11.8),
-               'area': SwathDefinition(lons, lats)}
+               'wavelength': WavelengthRange(9.8, 10.8, 11.8)}
     )
     scene.attrs['sensor'] = {'seviri'}
     return scene
